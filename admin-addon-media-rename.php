f9f1cb2017f82ef78f969714758b7614401d8214
Dávid Szabó <david.szabo97@gmail.com>
2017-05-17 22:46:10 +0200

    Fix #9 rename doesn't work with folder numeric prefixes

--- admin-addon-media-rename.php
+++ admin-addon-media-rename.php
@@ -27,42 +27,49 @@ class AdminAddonMediaRenamePlugin extends Plugin {
     }
 
     if ($this->grav['uri']->path() == self::PATH) {
-      // Make sure we have all the data we need
-      if (!isset($_POST['media_path']) || !isset($_POST['file_name']) || !isset($_POST['new_file_name'])) {
-        $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.INVALID_INPUT']));
-      }
-
-      $mediaPath = $_POST['media_path'];
-      $fileName = $_POST['file_name'];
-      $newFileName = $_POST['new_file_name'];
-      
-      // Only process changes
-      if ($fileName != $newFileName) {
-        // Locate the media file
-        $basePath = $this->grav['locator']->findResource('page://' . $mediaPath) . DS;
-
-        $filePath = $basePath . $fileName;
-        if (!file_exists($filePath)) {
-          $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.FILE_NOT_FOUND', $filePath]));
-        }
-
-        $newFilePath = $basePath . $newFileName;
-        if (!rename($filePath, $newFilePath)) {
-          $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.RENAME_FAILED', $filePath, $newFilePath]));
-        }
-
-        // Everything went fine
-        header('HTTP/1.1 200 OK');
-        die('{}');
-      } else {
-        $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.NO_CHANGES']));
-      }
+       $this->enable(['onPagesInitialized' => ['processRenameRequest', 0]]);
+       return;
     }
 
     $this->enable([
       'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
       'onTwigExtensions' => ['onTwigExtensions', 0]
     ]);
+  }
+
+  public function processRenameRequest() {
+    // Make sure we have all the data we need
+    if (!isset($_POST['media_path']) || !isset($_POST['file_name']) || !isset($_POST['new_file_name'])) {
+      $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.INVALID_INPUT']));
+    }
+
+    $mediaPath = $_POST['media_path'];
+    $fileName = $_POST['file_name'];
+    $newFileName = $_POST['new_file_name'];
+
+    $page = $this->grav['admin']->page(false, $mediaPath);
+    
+    // Only process changes
+    if ($fileName != $newFileName) {
+      // Locate the media file
+      $basePath = $page->path() . DS;
+
+      $filePath = $basePath . $fileName;
+      if (!file_exists($filePath)) {
+        $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.FILE_NOT_FOUND', $filePath]));
+      }
+
+      $newFilePath = $basePath . $newFileName;
+      if (!rename($filePath, $newFilePath)) {
+        $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.RENAME_FAILED', $filePath, $newFilePath]));
+      }
+
+      // Everything went fine
+      header('HTTP/1.1 200 OK');
+      die('{}');
+    } else {
+      $this->outputError($this->grav['language']->translate(['PLUGIN_ADMIN_ADDON_MEDIA_RENAME.ERROR.NO_CHANGES']));
+    }
   }
 
   public function onTwigTemplatePaths() {
